bd857f811912a4caa99f46a5faef03230e73eff1
Nicholas Herson <nicholas.herson@gmail.com>
2014-08-09 17:10:31 -0700

    bump to version 1.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nicholas.herson@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures farnsworth'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'java'
 depends 'python'
